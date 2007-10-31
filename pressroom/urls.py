2721b790018b94891ba058357d918aaac315b937
justin.driscoll <justin.driscoll@03f1e0f1-f332-0410-89a2-9b49dfe44f8c>
2007-10-31 18:30:50 +0000

    Included a preference option for including the object_list in the archive_year views for both Articles and Documents from a suggestion by Micheal Thornhill.
    git-svn-id: http://django-pressroom.googlecode.com/svn/trunk@44 03f1e0f1-f332-0410-89a2-9b49dfe44f8c

--- pressroom/urls.py
+++ pressroom/urls.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django_apps.pressroom.models import *
+
+try:
+    MAKE_ARTICLE_OBJECT_LIST = settings.PRESSROOM_MAKE_ARTICLE_OBJECT_LIST
+except:
+    MAKE_ARTICLE_OBJECT_LIST = False
+
+try:
+    MAKE_DOCUMENT_OBJECT_LIST = settings.PRESSROOM_MAKE_DOCUMENT_OBJECT_LIST
+except:
+    MAKE_DOCUMENT_OBJECT_LIST = False
 
 # custom views
 urlpatterns = patterns('django_apps.pressroom.views',
@@ -10,11 +21,16 @@ urlpatterns = patterns('django_apps.pressroom.views',
 
 # articles
 article_args = {'date_field': 'pub_date', 'allow_empty': True, 'queryset': Article.objects.get_published()}
+if MAKE_ARTICLE_OBJECT_LIST:
+    year_article_args = dict(article_args, make_object_list=True)
+else:
+    year_article_args = article_args
+
 urlpatterns += patterns('django.views.generic.date_based',
     url(r'^article/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$', 'object_detail', {'date_field': 'pub_date', 'slug_field': 'slug', 'queryset': Article.objects.all()}, name='pr-article-detail'),
     url(r'^article/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', article_args, name='pr-article-archive-day'),
     url(r'^article/(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', article_args, name='pr-article-archive-month'),
-    url(r'^article/(?P<year>\d{4})/$', 'archive_year', article_args, name='pr-article-archive-year'),
+    url(r'^article/(?P<year>\d{4})/$', 'archive_year', year_article_args, name='pr-article-archive-year'),
     url(r'^article/$', 'archive_index', article_args, name='pr-article-archive'),
 )
 urlpatterns += patterns('django.views.generic.list_detail',
@@ -23,11 +39,15 @@ urlpatterns += patterns('django.views.generic.list_detail',
 
 # documents
 document_args = {'date_field': 'pub_date', 'allow_empty': True, 'queryset': Document.objects.all()}
+if MAKE_ARTICLE_OBJECT_LIST:
+    year_document_args = dict(document_args, make_object_list=True)
+else:
+    year_document_args = article_args
 urlpatterns += patterns('django.views.generic.date_based',
     url(r'^document/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$', 'object_detail', dict(document_args, slug_field='slug'), name='pr-document-detail'),
     url(r'^document/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', document_args, name='pr-document-archive-day'),
     url(r'^document/(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', document_args, name='pr-document-archive-month'),
-    url(r'^document/(?P<year>\d{4})/$', 'archive_year', document_args, name='pr-document-archive-year'),
+    url(r'^document/(?P<year>\d{4})/$', 'archive_year', year_document_args, name='pr-document-archive-year'),
     url(r'^document/$', 'archive_index', document_args, name='pr-document-archive'),
 )
 urlpatterns += patterns('django.views.generic.list_detail',
