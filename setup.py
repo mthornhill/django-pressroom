3bb1d4a920f76fb28dcab57e7f84aca530aa73c9
michael.thornhill <michael.thornhill@03f1e0f1-f332-0410-89a2-9b49dfe44f8c>
2010-06-29 17:06:12 +0000

    improved setup file for distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "django-pressroom",
@@ -6,8 +10,23 @@ setup(
     url = 'http://code.google.com/p/django-pressroom/',
     license = 'BSD',
     description = "A pressroom application for django.",
-    author = 'Justin Driscoll',
+    long_description = read('README'),
+
+    author = 'Justin Driscoll, Michael Thornhill',
+    author_email = 'michael@maithu.com',
+
     packages = find_packages('src'),
     package_dir = {'': 'src'},
+
     install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
 )
