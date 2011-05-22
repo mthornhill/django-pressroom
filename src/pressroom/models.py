8ac852c93123d2c7a0d16b0b0e273c8ae739a064
Marcos Daniel Petry <marcospetry@gmail.com>
2011-05-22 00:54:38 -0300

    added i18n to models

--- src/pressroom/models.py
+++ src/pressroom/models.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.contrib.comments.moderation import CommentModerator, moderator
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # other imports
 from photologue.models import Gallery, Photo
@@ -25,18 +27,18 @@ class ArticleManager(models.Manager):
         return self.filter(publish=False)
 
 class Article(models.Model):
-    pub_date = models.DateTimeField("Publish date", default=datetime.now)
-    headline = models.CharField(max_length=200)
-    slug = models.SlugField(help_text='A "Slug" is a unique URL-friendly title for an object.')
-    summary = models.TextField(help_text="A single paragraph summary or preview of the article.")
-    body = models.TextField("Body text")
-    author = models.CharField(max_length=100)
-    publish = models.BooleanField("Publish on site", default=True,
-                                  help_text='Articles will not appear on the site until their "publish date".')
-    sections = models.ManyToManyField('Section', related_name='articles')
-    photos = models.ManyToManyField(Photo, related_name='articles', null=True, blank=True)
-    documents = models.ManyToManyField('Document', related_name='articles', null=True, blank=True)
-    enable_comments = models.BooleanField(default=True)
+    pub_date = models.DateTimeField(_("publish date"), default=datetime.now)
+    headline = models.CharField(_("headline"),max_length=200)
+    slug = models.SlugField(help_text=_('A "Slug" is a unique URL-friendly title for an object.'))
+    summary = models.TextField(help_text=_("A single paragraph summary or preview of the article."))
+    body = models.TextField(_("body text"))
+    author = models.CharField(_("author"), max_length=100)
+    publish = models.BooleanField(_("publish on site"), default=True,
+                                  help_text=_('Articles will not appear on the site until their "publish date".'))
+    sections = models.ManyToManyField('Section', related_name='articles', verbose_name=_('sections'))
+    photos = models.ManyToManyField(Photo, related_name='articles', null=True, blank=True, verbose_name=_('photos'))
+    documents = models.ManyToManyField('Document', related_name='articles', null=True, blank=True, verbose_name=_('documents'))
+    enable_comments = models.BooleanField(_('enable comments'),default=True)
 
     # Custom article manager
     objects = ArticleManager()
@@ -44,7 +46,10 @@ class Article(models.Model):
     class Meta:
         ordering = ['-pub_date']
         get_latest_by = 'pub_date'
-    
+        verbose_name = _('article')
+        verbose_name_plural = _('articles')
+        
+
     def __unicode__(self):
         return self.headline
 
@@ -63,15 +68,17 @@ if Article not in moderator._registry:
     moderator.register(Article, ArticleCommentModerator)
 
 class Document(models.Model):
-    file = models.FileField("Document", upload_to=PRESSROOM_DIR+"/documents/%Y/%b/%d")
-    pub_date = models.DateTimeField("Date published", default=datetime.now)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField()
-    summary = models.TextField()
+    file = models.FileField(_("document"), upload_to=PRESSROOM_DIR+"/documents/%Y/%b/%d")
+    pub_date = models.DateTimeField(_("date published"), default=datetime.now)
+    title = models.CharField(_('title'), max_length=200)
+    slug = models.SlugField(_('slug'))
+    summary = models.TextField(_('summary'))
 
     class Meta:
         ordering = ['-pub_date']
         get_latest_by = 'pub_date'
+        verbose_name = _('document')
+        verbose_name_plural = _('documents')
 
     def __unicode__(self):
         return self.title
@@ -94,11 +101,13 @@ class Document(models.Model):
                     os.removedirs(doc_dir)
 
 class Section(models.Model):
-    title = models.CharField(max_length=80, unique=True)
-    slug = models.SlugField()
+    title = models.CharField(_('title'), max_length=80, unique=True)
+    slug = models.SlugField(_('slug'))
 
     class Meta:
         ordering = ['title']
+        verbose_name = _('section')
+        verbose_name_plural = _('sections')
 
     def __unicode__(self):
         return self.title
