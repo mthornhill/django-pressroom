53e643ff302f9aa2ae5412be867671c2fc9d6225
Michael Thornhill <michael.thornhill@gmail.com>
2012-07-19 16:18:21 +0100

    fix up month views and url matching

--- src/pressroom/urls.py
+++ src/pressroom/urls.py
@@ -22,9 +22,9 @@ urlpatterns = patterns('pressroom.views',
 
 # articles
 urlpatterns += patterns('',
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<language>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/$', ArticleDetailView.as_view(slug_field='slug', queryset= Article.objects.get_published()), name='pr-article-detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', ArticleDayArchiveView.as_view(date_field='pub_date', allow_empty=True, queryset= Article.objects.get_published()), name='pr-article-archive-day'),
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', ArticleMonthArchiveView.as_view(date_field='pub_date', allow_empty=True, queryset= Article.objects.get_published()), name='pr-article-archive-month'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{1,2})/(?P<day>\w{1,2})/(?P<language>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/$', ArticleDetailView.as_view(slug_field='slug', queryset= Article.objects.get_published()), name='pr-article-detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{1,2})/(?P<day>\w{1,2})/$', ArticleDayArchiveView.as_view(date_field='pub_date', allow_empty=True, queryset= Article.objects.get_published()), name='pr-article-archive-day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{1,2})/$', ArticleMonthArchiveView.as_view(date_field='pub_date', allow_empty=True, queryset= Article.objects.get_published()), name='pr-article-archive-month'),
     url(r'^(?P<year>\d{4})/$', ArticleYearArchiveView.as_view(date_field='pub_date', allow_empty=True, queryset= Article.objects.get_published(), make_object_list=True), name='pr-article-archive-year'),
     url(r'^$', ArticleArchiveIndexView.as_view(date_field='pub_date', allow_future=False, allow_empty=True, queryset= Article.objects.get_published()), name='pr-article-archive'),
 )
