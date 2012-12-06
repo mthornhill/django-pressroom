374b4f793525c7f18e42fa4abaf26dd6939a4deb
Marcos Daniel Petry <marcospetry@gmail.com>
2012-12-05 23:52:54 -0200

    imperavi and ajax-selects are now an optional dependency for the project

--- src/pressroom/admin.py
+++ src/pressroom/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
+from django.conf import settings
 from models import Article, Document, Section
 from imperavi.admin import ImperaviAdmin
 from ajax_select import make_ajax_form
 from ajax_select.admin import AjaxSelectAdmin
 
-class ArticleAdmin(ImperaviAdmin, AjaxSelectAdmin):
+class ArticleAdminBase(admin.ModelAdmin):
     list_display = ('headline', 'author', 'language', 'pub_date', 'publish', 'modified_by')
     search_fields = ('headline', 'author', 'summary', 'body')
     list_filter = ['pub_date', 'author', 'language']
     date_hierarchy = 'pub_date'
     save_on_top = True
     filter_horizontal=('sections',)
-
-    form = make_ajax_form(Article,{'photos':'photos', 'documents': 'documents', 'translation_of': 'articles'})
 
     def save_model(self, request, obj, form, change):
         obj.modified_by = request.user
@@ -29,7 +28,7 @@ class ArticleAdmin(ImperaviAdmin, AjaxSelectAdmin):
                     elif request.user.username:
                         author = request.user.username
             kwargs['initial'] = author
-        return super(ArticleAdmin, self).formfield_for_dbfield(field, **kwargs)
+        return super(ArticleAdminBase, self).formfield_for_dbfield(field, **kwargs)
 
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
@@ -37,6 +36,20 @@ class DocumentAdmin(admin.ModelAdmin):
 
 class SectionAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+if 'imperavi' in settings.INSTALLED_APPS and 'ajax_select' in settings.INSTALLED_APPS:
+    class ArticleAdmin(ArticleAdminBase, ImperaviAdmin, AjaxSelectAdmin):
+        form = make_ajax_form(
+            model=Article,
+            fieldlist={
+                'photos':'photos',
+                'documents': 'documents',
+                'translation_of': 'articles'
+            }
+        )
+else:
+    class ArticleAdmin(ArticleAdminBase):
+        pass
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Document, DocumentAdmin)
